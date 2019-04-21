12a7a9c98e8d49575ed965a3bf3510eebbef6757
xjphongx <JimmyPhong16@csu.fullerton.edu>
2019-04-21 12:53:28 -0700

    added and studied two delete node functions

--- name.cpp
+++ name.cpp
@@ -23,6 +23,14 @@ class list
             head = nullptr;
             tail = nullptr;
         }
+        void addFront(string n, int a)
+        {
+            node * temp = new node;
+            temp->age = a;
+            temp->name = n;
+            temp -> next = head;
+            temp = head;
+        }
         void addEnd(string n, int a)
         {
             node*temp = new node;
@@ -41,38 +49,93 @@ class list
             }
         
          }
+        void deleteFront()
+        {
+            node * temp = new node;
+            temp = head; // head will not point to the next node but 
+                        //will point to temp instead
+            head= head->next; // makes next node the head
+            delete temp;
+        }
+        void deleteEnd()
+        {//need two node ptrs
+            node* current = new node;
+            node* prev = new node;
+            current = head;
+            while(current->next != nullptr)
+            {
+                prev = current;
+                current = current->next;
+            }
+            //make the tail point to what prev points to
+            tail = prev;//this becomes the new tail
+            prev->next = nullptr; //does not point to tail
+            delete current;
+            
+        }
+
+
+
+
          void display()
          {
             node * cursor = new node;
             cursor = head; //starting point
+            cout<<"\tName\tAge"<<endl;
+            cout <<"--------------------"<<endl;
             while (cursor != nullptr)
             {
-                cout<< cursor->name << " " << cursor->age << endl;
+                cout<< cursor->name << "\t" << cursor->age << endl;
                 cursor = cursor->next;//goes to the next ptr
             }
-
-
          }
+        void findOldest()
+        {//traverse the list with a cursor
+            node * cursor = new node;
+            cursor = head;
+            int oldestAge=0;
+            string nameOfOldest;
+            while (cursor != nullptr)
+            {
+
+                if(cursor->age > oldestAge)
+                {
+                    oldestAge = cursor->age;
+                    nameOfOldest = cursor->name;
+                
+                }
+                cursor = cursor->next;//moves to the next ptr
+
+            }
+            cout << "The oldest person is "<< nameOfOldest <<endl;
+
+        }
+         
 
 
 
 
 };
 
-
+//***********************
 void readData(list & l);
-
-
 int main()
 {
 
-
-    cout<< "hi"<<endl;
     list LinkedList;
 
     readData(LinkedList);
     LinkedList.display();
+    LinkedList.findOldest();
 
+    LinkedList.deleteFront();
+    LinkedList.deleteFront();
+    LinkedList.display();
+    LinkedList.findOldest();
+
+    LinkedList.deleteEnd();
+    LinkedList.display();
+    LinkedList.findOldest();
 
     return 0;
 }
@@ -94,8 +157,5 @@ void readData(list & LinkedList)
         LinkedList.addEnd(name, age);
 
     }
-
-
-
     inputFile.close();
 }
