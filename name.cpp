23f4432d036332d9390461906cc18998ca2076d7
xjphongx <JimmyPhong16@csu.fullerton.edu>
2019-04-21 11:29:40 -0700

    updated with the andEnd function and readData function

--- name.cpp
+++ name.cpp
@@ -17,12 +17,42 @@ class list
     private:
         node * head;
         node*tail;
-    
-    list()
-    {
-        head = nullptr;
-        tail = nullptr;
-    }
+    public:
+        list()
+        {
+            head = nullptr;
+            tail = nullptr;
+        }
+        void addEnd(string n, int a)
+        {
+            node*temp = new node;
+            temp->name = n;
+            temp->age=a;
+           if(head == nullptr)
+           {
+               head = temp;
+               tail = temp;
+               temp = nullptr;
+            }
+            else
+            {
+                tail->next= temp; //i want temp to point to whatever tail is ptring to
+                tail = temp; //set tail to be the temp node for next time
+            }
+        
+         }
+         void display()
+         {
+            node * cursor = new node;
+            cursor = head; //starting point
+            while (cursor != nullptr)
+            {
+                cout<< cursor->name << " " << cursor->age << endl;
+                cursor = cursor->next;//goes to the next ptr
+            }
+
+
+         }
 
 
 
@@ -30,33 +60,42 @@ class list
 };
 
 
-void readData();
+void readData(list & l);
 
 
 int main()
 {
 
 
-cout<< "hi"<<endl;
+    cout<< "hi"<<endl;
+    list LinkedList;
 
+    readData(LinkedList);
+    LinkedList.display();
 
 
     return 0;
 }
-void readData()
+
+void readData(list & LinkedList)
 {//reads from a txt file
     ifstream inputFile;
     inputFile.open("pro5.txt");
-    int counter = 0;
+    //int counter = 0;
+    //string nameArray[counter];
+    //int ageArray[counter];//the purpose of this is to set both array index to match each other
+    string name;
+    int age;
+    
     while(!inputFile.eof())
     {
-        counter++;
+        inputFile >> name;
+        inputFile >> age;
+        LinkedList.addEnd(name, age);
+
     }
 
-    string array[counter];
-
-    
 
 
-
+    inputFile.close();
 }
